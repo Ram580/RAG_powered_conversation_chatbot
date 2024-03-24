82e03dde5cee0eafa35e9ca4af518c5a01f8e8ec
Ram580 <ramhemanth580@gmail.com>
2024-03-25 00:52:11 +0530

    imported libraries

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+
+import os
+import google.generativeai as genai
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.memory import ConversationBufferMemory 
+from langchain.chains import ConversationalRetrievalChain
+
+
+# Parsing the uploaded documents and creating a single text blob
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+# creating chunks of the text blob
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# Create Embeddings using Huggingface Embeddings
+import sentence_transformers
+from langchain.embeddings import HuggingFaceEmbeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
