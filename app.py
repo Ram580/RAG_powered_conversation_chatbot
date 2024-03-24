083c486fb4a39ed2a461839932a65c321eea06fe
Ram580 <ramhemanth580@gmail.com>
2024-03-25 01:21:38 +0530

    working on helper functions

--- app.py
+++ app.py
@@ -32,3 +32,30 @@ def get_text_chunks(text):
 import sentence_transformers
 from langchain.embeddings import HuggingFaceEmbeddings
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+# Load environment variables to get Pinecone API Key and env
+load_dotenv()
+# Access the value of PINECONE_API_KEY
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_API_ENV = os.getenv("PINECONE_API_ENV")
+
+# initialize pinecone
+import pinecone
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment=PINECONE_API_ENV  # next to api key in console
+)
+index_name = "rag-chatbot" # put in the name of your pinecone index here
+
+
+#  Function that indexes documents into Pinecone
+from langchain.vectorstores import Pinecone
+
+# Load the data into pinecone database
+def get_vector_store(text_chunks):
+   #docsearch = Pinecone.from_texts(chunked_data, embeddings, index_name=index_name)
+   index = Pinecone.from_texts([t for t in text_chunks], embeddings, index_name=index_name)
+   return index
+
